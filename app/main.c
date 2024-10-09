acf8f064111ec313acc0d8234fcf47c8e9ce9231
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-10-09 12:40:39 +0000

    command not found

--- app/main.c
+++ app/main.c
@@ -1,11 +1,18 @@
+
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  setbuf(stdin, NULL); setbuf(stdout, NULL);
   printf("$ ");
-  fflush(stdout);
+  fflush(stdout); 
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
+  fgets(input, 100, stdin); // stdin에서 input에 100 char 읽어옴
+  input[strlen(input) - 1] = '\0';
+
+  fprintf(stdout, "%s: command not found\n", input);
+  
   return 0;
 }
