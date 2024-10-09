ea60f26c91191cdd547f6f403ee0559f65be4d76
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-10-09 13:24:25 +0000

    type

--- app/main.c
+++ app/main.c
@@ -22,20 +22,36 @@ int main() {
     input[input_len - 1] = '\0'; // ex - strlen(asdf\n) 은 5 반환. \n를 \0로 변경하기 위해서 4 번째 자리에 \0 넣어야.
 
     // exit
-    char* cmd_match  = strstr(input, "exit");
-    if (cmd_match != NULL) {
+    if (strncmp(input, "exit", 4) == 0) {
       char* sep = strchr(input, ' ');
       int exit_num = atoi(sep + 1); // sep + 1 지점부터 문자열 끝까지를 정수화
       return exit_num;
     }
+    
 
     // echo
-    cmd_match = strstr(input, "echo");
-    if (cmd_match != NULL) {
+    if (strncmp(input, "echo", 4) == 0) {
       // echo hello world -> print hello world
       char* sep = strchr(input, ' ');
       sep++;
       printf("%s\n", sep);
+      continue;
+    }
+
+    // type
+    if (strncmp(input, "type", 4) == 0) {
+      char* sep = strchr(input, ' ');
+      sep++;
+      char buf[100];
+      sprintf(buf, "%s", sep);
+      if (strncmp(buf, "type", 4) == 0 || 
+          strncmp(buf, "echo", 4) == 0 || 
+          strncmp(buf, "exit", 4) == 0) {
+          fprintf(stdout, "%s is a shell builtin\n", buf);
+        } else {
+          fprintf(stdout, "%s: not found\n", buf);
+        }
+
       continue;
     }
 
