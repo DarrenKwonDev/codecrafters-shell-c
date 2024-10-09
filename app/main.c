9cab768a0f27b17b2d0e55835982b3660d3a2fa4
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-10-09 12:45:45 +0000

    comment

--- app/main.c
+++ app/main.c
@@ -10,7 +10,7 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin); // stdin에서 input에 100 char 읽어옴
-  input[strlen(input) - 1] = '\0';
+  input[strlen(input) - 1] = '\0'; // ex - strlen(asdf\n) 은 5 반환. \n를 \0로 변경하기 위해서 4 번째 자리에 \0 넣어야.
 
   fprintf(stdout, "%s: command not found\n", input);
   
