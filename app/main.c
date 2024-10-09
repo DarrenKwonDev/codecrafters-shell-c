bfc7221789baf7477044e772a0d11f6b30d670d1
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-10-09 12:43:00 +0000

    .

--- app/main.c
+++ app/main.c
@@ -7,10 +7,9 @@ int main() {
   printf("$ ");
   fflush(stdout); 
 
-  // Wait for user input
   char input[100];
   fgets(input, 100, stdin); // stdin에서 input에 100 char 읽어옴
-  input[strlen(input) - 1] = '\0';
+  input[strlen(input) - 1] = '\0'; // ex - strlen(asdf\n) = 5. 따라서 4번째 자리에 null char 를 포함해야 함
 
   fprintf(stdout, "%s: command not found\n", input);
   
