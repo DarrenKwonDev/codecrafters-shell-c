b2c780b8fda29e8c90a737a447c5f08a6ac51be1
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-10-10 20:14:25 +0000

    fork and exec

--- app/main.c
+++ app/main.c
@@ -1,14 +1,14 @@
 
-#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <sys/wait.h>
 
 #define MAX_PATH 1024
 #define MAX_COMMAND 256
-
 
 int find_command(const char* command, char* result) {
     char* path = getenv("PATH");
@@ -17,7 +17,7 @@ int find_command(const char* command, char* result) {
     }
 
     char* path_copy = strdup(path); // internal malloc. free 해야 함.
-    char* dir = strtok(path_copy, ":");
+    char* dir       = strtok(path_copy, ":");
 
     while (dir != NULL) {
         char full_path[MAX_PATH];
@@ -36,67 +36,73 @@ int find_command(const char* command, char* result) {
     return 0;
 }
 
+int main(int argc, char** argv) {
+    setbuf(stdin, NULL);
+    setbuf(stdout, NULL);
+    char input[100];
 
-int main() {
-  setbuf(stdin, NULL); 
-  setbuf(stdout, NULL);
-  char input[100];
-  
-  // REPL
-  while (1)
-  {
-    memset(input, 0, sizeof(input));
+    // REPL
+    while (1) {
+        memset(input, 0, sizeof(input));
 
-    printf("$ ");
-    fflush(stdout); 
+        printf("$ ");
+        fflush(stdout);
 
-    fgets(input, 100, stdin); // stdin에서 input에 100 char 읽어옴
-    int input_len = strlen(input);
-    input[input_len - 1] = '\0'; // ex - strlen(asdf\n) 은 5 반환. \n를 \0로 변경하기 위해서 4 번째 자리에 \0 넣어야.
+        fgets(input, 100, stdin); // stdin에서 input에 100 char 읽어옴
+        int input_len = strlen(input);
+        input[input_len - 1] = '\0'; // ex - strlen(asdf\n) 은 5 반환. \n를 \0로 변경하기 위해서 4
+                                     // 번째 자리에 \0 넣어야.
 
-    // exit
-    if (strncmp(input, "exit", 4) == 0) {
-      char* sep = strchr(input, ' ');
-      int exit_num = atoi(sep + 1); // sep + 1 지점부터 문자열 끝까지를 정수화
-      return exit_num;
-    }
-    
-
-    // echo
-    if (strncmp(input, "echo", 4) == 0) {
-      // echo hello world -> print hello world
-      char* sep = strchr(input, ' ');
-      sep++;
-      printf("%s\n", sep);
-      continue;
-    }
-
-    // type
-    if (strncmp(input, "type", 4) == 0) {
-      char* sep = strchr(input, ' ');
-      sep++;
-      char command[MAX_COMMAND];
-      strcpy(command, sep);
-      
-      if (strncmp(command, "type", 4) == 0 || 
-          strncmp(command, "echo", 4) == 0 || 
-          strncmp(command, "exit", 4) == 0) {
-          fprintf(stdout, "%s is a shell builtin\n", command);
-        } else {
-          char result[MAX_PATH];
-          if (find_command(command, result)) {
-              fprintf(stdout, "%s is %s\n", command, result);
-          } else {
-              fprintf(stdout, "%s: not found\n", command);
-          }
+        // exit
+        if (strncmp(input, "exit", 4) == 0) {
+            char* sep      = strchr(input, ' ');
+            int   exit_num = atoi(sep + 1); // sep + 1 지점부터 문자열 끝까지를 정수화
+            return exit_num;
         }
 
-      continue;
+        // echo
+        if (strncmp(input, "echo", 4) == 0) {
+            // echo hello world -> print hello world
+            char* sep = strchr(input, ' ');
+            sep++;
+            printf("%s\n", sep);
+            continue;
+        }
+
+        // type
+        if (strncmp(input, "type", 4) == 0) {
+            char* sep = strchr(input, ' ');
+            sep++;
+            char command[MAX_COMMAND];
+            strcpy(command, sep);
+
+            if (strncmp(command, "type", 4) == 0 || strncmp(command, "echo", 4) == 0 ||
+                strncmp(command, "exit", 4) == 0) {
+                fprintf(stdout, "%s is a shell builtin\n", command);
+            } else {
+                char result[MAX_PATH];
+                if (find_command(command, result)) {
+                    // command를 arguments와 함께 실행시키고 싶음.
+                    // fork and exec 패턴
+                    pid_t pid = fork();
+                    if (pid == 0) { // child proc
+                        execv(command, argv++);
+                    } else { // parent proc
+                        int status;
+                        waitpid(pid, &status, 0);
+                    }
+
+                } else {
+                    fprintf(stdout, "%s: not found\n", command);
+                }
+            }
+
+            continue;
+        }
+
+        // not found
+        fprintf(stdout, "%s: command not found\n", input);
     }
 
-    // not found
-    fprintf(stdout, "%s: command not found\n", input);
-  }
-
-  return 0;
+    return 0;
 }
